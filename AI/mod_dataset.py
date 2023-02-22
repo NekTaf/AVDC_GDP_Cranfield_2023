4d5cf19450afc569743f52cac4098201258584a4
NekTaf <72943822+NekTaf@users.noreply.github.com>
2023-02-22 23:34:30 +0000

    Yolo Train and Database Modification code without comments

---
+++ AI/mod_dataset.py
@@ -0,0 +1,32 @@
+import os
+
+# assign directory
+directory = ['test/labels','train/labels','valid/labels']
+#directory = ['files']
+
+for dire in directory:
+    for filename in os.listdir(dire):
+        file = os.path.join(dire, filename)
+
+        #if os.path.isfile(file):
+            #print(file)
+
+        with open(file, "r") as f:
+
+            lines = f.readlines()
+
+        with open(file, "w") as f:
+
+            for line in lines:
+
+                if line.startswith("0") or line.startswith("4"):
+
+                    words = line.split()
+                    words[0] = "0"
+                    new_line = " ".join(words)
+                    #print(new_line)
+
+                    f.write(new_line+'\n')
+        i=+1
+    print(i)
+
